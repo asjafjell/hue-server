b915795a23ab7b937f92f94c283f8b8d4b08b86a
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2018-10-14 18:20:26 +0200

    Refaktorerer exports til bunnen for bedre lesbarhet

--- public/javascript/bathroom/dimmer.js
+++ public/javascript/bathroom/dimmer.js
@@ -1,28 +1,35 @@
 const moment = require('moment');
+const connector = require('./connector');
 
 const PHILIPS_HUE_DATE_FORMAT = 'YYYY-MM-DDTHH:mm:ss';
 const USED_RECENTLY_MINUTES = 30;
 
-module.exports = {
 
-    isButtonUsedRecently: function (button, currentTime) {
-        const triggeredAt = toMoment(button.lasttriggered);
-        const diff = moment.duration(currentTime.diff(triggeredAt));
+function isButtonUsedRecently(button, currentTime) {
+    const triggeredAt = toMoment(button.lasttriggered);
+    const diff = moment.duration(currentTime.diff(triggeredAt));
 
-        return diff.asMinutes() < Math.abs(USED_RECENTLY_MINUTES);
+    return diff.asMinutes() < Math.abs(USED_RECENTLY_MINUTES);
 
-    },
+}
 
-    areButtonsUsedRecently: function (buttons, currentTime) {
-        for (const i in buttons) {
-            if (module.exports.isButtonUsedRecently(buttons[i], currentTime)) {
-                return true;
-            }
+function areButtonsUsedRecently (buttons, currentTime) {
+    for (const i in buttons) {
+        if (module.exports.isButtonUsedRecently(buttons[i], currentTime)) {
+            return true;
         }
-        return false;
     }
+    return false;
+}
+
+function isSwitchUsedRecently({ dimmerName }){
 }
 
 function toMoment(rawDate) {
     return moment.utc(rawDate, PHILIPS_HUE_DATE_FORMAT)
+}
+
+module.exports = {
+    isButtonUsedRecently: isButtonUsedRecently,
+    areButtonsUsedRecently: areButtonsUsedRecently
 }
