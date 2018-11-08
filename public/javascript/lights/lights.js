1d41584011ff86983cefbb5607eb89eed3c53f61
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2018-11-08 20:22:06 +0100

    Legger inn støtte for avbryting av regler ved manuell overkjøring

--- public/javascript/lights/lights.js
+++ public/javascript/lights/lights.js
@@ -2,19 +2,49 @@ const connector = require('../connector/connector');
 const daylight = require('../connector/daylight');
 const moment = require('moment');
 
-async function setLightPercentForGroup({groupName}) {
+const MINUTES_TO_MAKE_RULES_ABORT_ADJUSTMENT = 30;
+
+async function abortAdjustment({rulesThatAbort}) {
+    if (rulesThatAbort === undefined) {
+        return false;
+    }
+
+    const rulesTriggered = await connector.getRulesTriggered({timespanInMinutes: MINUTES_TO_MAKE_RULES_ABORT_ADJUSTMENT});
+    const filtered = rulesTriggered.filter(r => rulesThatAbort.includes(parseInt(r.key)));
+
+    if (filtered.length > 0) {
+        console.log('   -> No adjustments, Manual override with rules ' + filtered.map(rule => rule.key).toString());
+        return true;
+    }
+
+    return false;
+}
+
+async function setLightPercentForGroup({groupName, rulesTriggeredAbortingAdjustment}) {
+
     const currentBrightness = await connector.getLightBrightness({groupName});
 
     const naturalLight = await daylight.calculateDaylightInPercent({now: moment()});
     const baseLight = await connector.getLightBrightness({groupName: 'Livingroom'});
+
+    console.log(groupName + ': Natural light: ' + naturalLight + '%, baseLight: ' + baseLight + '%.');
+    console.log('   Current brightness: ' + currentBrightness + '%.');
+
+    const manualOverride = await abortAdjustment({rulesThatAbort: rulesTriggeredAbortingAdjustment});
+    if (manualOverride) {
+        return;
+    }
 
     const minimumValue = 15;
     let nextBrightness = Math.max(naturalLight, baseLight, minimumValue);
 
     const noBrightnessChanges = currentBrightness === nextBrightness;
     if (noBrightnessChanges) {
+        console.log('   -> No adjustments. Current brightness is same as new. ');
         return;
     }
+
+    console.log('   -> Adjusting to ' + nextBrightness + '%');
 
     await connector.setLightBrightnessByGroupName({
             groupName: groupName,
@@ -22,8 +52,6 @@ async function setLightPercentForGroup({groupName}) {
         }
     );
 
-    console.log(groupName + ': Natural light: ' + naturalLight + '%, baseLight: ' + baseLight + '%');
-    console.log(groupName + ': Adjusting to ' + nextBrightness + '%')
 
 }
 
--- routes/index.js
+++ routes/index.js
@@ -9,8 +9,12 @@ const lights = require('../public/javascript/lights/lights');
 scheduler.schedule('0-59/10 * * * * *', async function () {
     console.log('Running recurring light adjustment at ' + moment().toISOString());
 
-    await lights.setLightPercentForGroup({groupName: "Kitchen"});
-    await lights.setLightPercentForGroup({groupName: "Walk in closet"});
+    await lights.setLightPercentForGroup({groupName: "Kitchen",});
+    await lights.setLightPercentForGroup({groupName: "Hallway closet"});
+    await lights.setLightPercentForGroup({
+        groupName: "Bathroom",
+        rulesTriggeredAbortingAdjustment: [34, 39, 42, 45, 46, 110]
+    })
 });
 
 // Get rules triggered the last minute
@@ -18,15 +22,15 @@ scheduler.schedule('0-59/10 * * * * *', async function () {
     const rulesTriggered = await connector.getRulesTriggered({timespanInMinutes: 1});
 
     if (rulesTriggered.length > 0) {
-        //console.log('Found rules triggered ' + rulesTriggered.map(rule => rule.key).toString());
+        console.log('Found rules triggered ' + rulesTriggered.map(rule => rule.key).toString());
     } else {
     }
 });
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+    res.render('index', {title: 'Express'});
 });
 
 module.exports = router;
